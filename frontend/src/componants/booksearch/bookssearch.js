ca49d1b9cfa831a81b8ffb9b7a59ac8a682ee893
rohanga <30858021+rohanga@users.noreply.github.com>
2024-11-20 21:50:36 +0530

    Update bookssearch.js

--- frontend/src/componants/booksearch/bookssearch.js
+++ frontend/src/componants/booksearch/bookssearch.js
@@ -11,7 +11,7 @@ function SearchBooks({ userId,onAddBook }) {
   const searchBooks = async () => {
     try {
       const response = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`
+        `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&key=${process.env.API_KEY}`
       );
       setBooks(response.data.items);
     } catch (error) {
